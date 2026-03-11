e6579acf71e06ba21303f87342c4e88e1726ecfa
ANA BEATRIZ RAMOS <ana.bramos4@senacsp.edu.br>
2026-03-10 21:42:00 -0300

    estilização da page404 e atividade, finalizadas!

--- PSY-TDS/src/pages/NotFound.tsx
+++ PSY-TDS/src/pages/NotFound.tsx
@@ -1,5 +1,7 @@
 import { useLocation  } from "react-router-dom";
 import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import { Bug } from "lucide-react"
 
 const NotFound = () => {
     const location = useLocation();
@@ -12,13 +14,25 @@ const NotFound = () => {
     }, [location.pathname]);
 
     return(
-        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="min-h-screen flex items-center justify-center bg-cyan-50 ">
             <div className="text-center">
-                <h1 className="text 4x1 font-bold mb-4">404</h1>
-                <p className="text-x1 text-gray-600 mb-4">Oops! Page not found</p>
-                <a href="/" className="text-blue-500 hover:text-blue-700 underline">
-                    Return to Home
+                <Bug 
+                size={60} 
+                className="text-pink-500 animate-bounce block mx-auto mb-4" 
+                />
+                <h1 className="text 4x1 font-bold mb-4 text-[45px] text-cyan-800 font-serif">Erro 404</h1>
+                <p className="text-x1 text-cyan-600 mb-4 font-serif">Parece que encontramos um pequeno intruso. 
+                    Esta página sumiu do mapa!</p>
+                
+                <Button 
+                className="bg-pink-500 h-auto py-4 px-8 hover:bg-pink-50 hover:text-pink-300 
+                hover:border-pink-700 transition-all rounded-2xl">
+
+                <a href="/" className="text-pink-300 hover:text-pink-500 text-[37px] rounded-md font-sans">
+                    Para retornar, clique aqui
                 </a>
+
+                </Button>
             </div>
         </div>
     );
