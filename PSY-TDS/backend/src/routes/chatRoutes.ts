eede91f2ff930ccbfff16fb4acca0009cb819c0c
ANA BEATRIZ RAMOS <ana.bramos4@senacsp.edu.br>
2026-03-17 20:18:08 -0300

    Trocando rota

--- PSY-TDS/backend/src/routes/chatRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { sendMessage } from './../controllers/chatControllers';
-
-const router = express.Router()
-
-router.post("/chat", sendMessage);
-
-export default  router;
